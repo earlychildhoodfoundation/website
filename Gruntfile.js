4d8ed47b27379bbae7306313c0f88401dd213935
Jan De Wilde <jan@jandewilde.com>
2017-03-15 15:23:22 -0400

    Fix for revving JS during build

--- Gruntfile.js
+++ Gruntfile.js
@@ -208,7 +208,7 @@ module.exports = function (grunt) {
 			dist: {
 				files: {
 					src: [
-						'<%= config.dist %>/scripts/{,*/}*.{js}',
+						'<%= config.dist %>/scripts/{,*/}*.js',
 						'<%= config.dist %>/styles/{,*/}*.css',
 						'<%= config.dist %>/images/{,*/}*.*',
 						'<%= config.dist %>/styles/fonts/{,*/}*.*',
