3dce155184dc03f6e3477e33c08c12aa3defa1cb
Jan De Wilde <jandw@mit.edu>
2017-03-14 20:58:31 -0400

    JSON for Grants Awarded + VueJS templates + Grants Awarded archive modal

---
+++ app/scripts/grants.js
@@ -0,0 +1,23 @@
+var grantsJSONUrl = 'scripts/grants.json';
+
+var grants = new Vue({
+  el: '#vueGrants',
+
+  data: {
+    grants: null
+  },
+
+  created: function () {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData: function () {
+      var self = this;
+      $.get( grantsJSONUrl, function(data){
+        self.grants = data.grants;
+      });
+    }
+  }
+
+});
