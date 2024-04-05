0b817dd4c5f715bf9bd3904f37f6dbe7ccade5a0
Jan De Wilde <jan@jandewilde.com>
2024-04-05 02:33:45 -0400

    Keep contact info as global data

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,15 @@ const currentYear = new Date().getFullYear();
 module.exports = {
   name: 'Caplan Foundation for Early Childhood',
   url: 'https://earlychildhoodfoundation.org',
-  email: 'info@earlychildhoodfoundation.org',
+  contact: {
+    name: 'Amanda Liedtka, CPA',
+    address: 'P.O. Box 746',
+    city: 'Lock Haven',
+    state: 'PA',
+    zip: '17745',
+    email: 'info@earlychildhoodfoundation.org',
+    phone: '570 484 5155',
+  },
   tagline:
     'The Caplan Foundation for Early Childhood provides grants for innovative, creative projects and programs that will significantly enhance the development, health, safety, education or quality of life of children from infancy through seven years of age.',
   imgDir: '/assets/images/',
