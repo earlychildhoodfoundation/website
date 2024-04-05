4afebfdb98fa1365c803054dc0d83c8a23d8e438
Jan De Wilde <jan@jandewilde.com>
2024-04-04 21:43:43 -0400

    WIP refactor index template

---
+++ src/_data/site.js
@@ -0,0 +1,13 @@
+const currentYear = new Date().getFullYear();
+
+module.exports = {
+  name: 'Caplan Foundation for Early Childhood',
+  url: 'https://earlychildhoodfoundation.org',
+  email: 'info@earlychildhoodfoundation.org',
+  tagline:
+    'The Caplan Foundation for Early Childhood provides grants for innovative, creative projects and programs that will significantly enhance the development, health, safety, education or quality of life of children from infancy through seven years of age.',
+  imgDir: '/assets/images/',
+  currentYear,
+  navigation: [{ label: '', url: '' }],
+  footerNavigation: [{ label: '', url: '' }],
+};
