d250199ec2a5bd45cbf73a1d1ce4c1a178ba1a19
Jan De Wilde <jan@jandewilde.com>
2024-04-21 09:55:38 -0400

    Footer WIP

--- src/_data/site.js
+++ src/_data/site.js
@@ -13,7 +13,7 @@ module.exports = {
     state: 'PA',
     zip: '17745',
     email: 'info@earlychildhoodfoundation.org',
-    phone: '570 484 5155',
+    phone: '+1 570 484 5155',
   },
   tagline:
     'The Caplan Foundation for Early Childhood provides grants for innovative, creative projects and programs that will significantly enhance the development, health, safety, education or quality of life of children from infancy through seven years of age.',
